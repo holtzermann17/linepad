899f632ac9232391924aed91cb811ec1f1783f7a
Joe Corneli <holtzermann17@gmail.com>
2011-03-27 22:22:08 +0100

    Added preliminary code for parsing and acting on changesets

--- linepad.el
+++ linepad.el
@@ -95,6 +95,12 @@ back, parse them and call the appropriate callbacks."
           (let* ((client-vars (plist-get response :collab_client_vars))
                  (init-text (plist-get client-vars :initialAttributedText)))
             (setq baserev (plist-get client-vars :rev))
+
+            ;; TODO: it would be good to get this set as a
+            ;; proper changeset, and to unpack it in the
+            ;; manner that is done below.  I believe I
+            ;; understand changesets well enough to know
+            ;; how to do this now.
             (setq changesets (cons (concat "Z:0>"
                                            (plist-get init-text :attribs)
                                            "$"
@@ -102,9 +108,12 @@ back, parse them and call the appropriate callbacks."
                                    changesets))))
 
         ;; ONGOING PHASE: keep track of any changesets
-        ;; that are coming from the server.
+        ;; that are coming from the server.  Actually
+        ;; keeping track of the "unpacked" versions at
+        ;; present (see below).
         (when (and data (plist-get data :changeset))
-          (setq changesets (cons (plist-get data :changeset)
+          (setq changesets (cons (linepad-unpack-changeset 
+                                  (plist-get data :changeset))
                                  changesets))
           (setq baserev (plist-get data :newRev)))
 
@@ -171,11 +180,180 @@ back, parse them and call the appropriate callbacks."
 ;; section for managing changesets
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-(defun linepad-unpack-changeset (changeset))
+;; I'm not actually sure that this string represents the
+;; digits of base 36, partly because there aren't 36
+;; things here.  I guess "10" is 36 in base 36...  but
+;; then you'd suppose "z" would be 35 and that's not what
+;; we get here.
+
+(defvar linepad-base-36-lookup 
+  (progn (setq count -1)
+         (map 'list
+              (lambda (str)
+                (setq count (1+ count))
+                (list str count))
+              (mapcar 'char-to-string 
+                      (append "0123456789abcdefghiklmnopqrstuvwxyz" nil)))))
+
+(defun base-36-string-to-number (str)
+;;  v = v * b + digit;
+  (let* ((input (reverse (mapcar 'char-to-string (append str nil))))
+         (sum (second (assoc (car input) linepad-base-36-lookup)))
+         (input (cdr input))
+         (base 1))
+    (while input
+      (setq sum (+ sum
+                   (* (second (assoc (car input) linepad-base-36-lookup))
+                      36)))
+      (setq base (1+ base))
+      (setq input (cdr input)))
+    sum))
+
+(base-36-string-to-number "1z")
+
+; (regexp-opt '("*" "+" "=" "|" ">" ":" "$") t)
+;; Based on `split-string' from subr.el
+(defun linepad-split-changeset (string)
+  (let ((rexp "\\([$*+:=>|]\\)")
+	(start 0)
+	notfirst
+        s
+	(list nil))
+    (while (and (string-match 
+                 rexp string
+                 (if (and notfirst
+                          (= start 
+                             (match-beginning 0))
+                          (< start (length string)))
+                     (1+ start) start))
+		(< start (length string)))
+      (setq notfirst t)
+      (setq s (match-string-no-properties 1))
+      (if (< start (match-beginning 0))
+	  (setq list
+		(cons (cons (substring string
+                                        (max (- start 1)
+                                             0)
+                                        start)
+                            (list 
+                             (substring string 
+                                 start
+                                 (match-beginning 0))
+                             ))
+		      list)))
+      (setq start (match-end 0)))
+    (if (< start (length string))
+	(setq list
+	      (cons (substring string start) list)))
+    ;; get rid of the Z
+    (cdr (nreverse list))))
+
+;; this code is presumably inefficient, should be done
+;; with nconc or whatever
+(defun linepad-unpack-changeset (changeset)
+  (mapcar (lambda (elt)
+            (if (listp elt)
+                (list (first elt) (base-36-string-to-number (second elt)))
+              elt))
+          (linepad-split-changeset changeset)))
+
+; (linepad-unpack-changeset "Z:1v>5|2=17=m*0+5$auoeu")
+; => ((":" 66) (">" 5) ("|" 2) ("=" 43) ("=" 21) ("*" 0) ("+" 5) "auoeu")
+
+;(linepad-changeset-to-elisp "Z:1v>5|2=17=m*0+5$auoeu")
+
+;; The generated routine will either take a form something like:
+;;   (progn (goto-char CHAR) (insert STRING))
+;; or else
+;;   (progn (goto-char CHAR) (delete-char N))
+;; which can be simplified to
+;;   (delete-region CHAR (+ N CHAR))
+
+(defun linepad-changeset-to-elisp (changeset)
+  (let* ((instructions (linepad-unpack-changeset changeset))
+         (bank (car (last instructions)))
+         routine
+         prev-op
+         prev-no)
+    (mapc (lambda (elt)
+            ;; trying this just to be able to use case with its `eql' comparison
+            (cond 
+              ;; purely for the sake of information, can
+              ;; presumably ignore
+              ((equal (car elt) ":") (setq prev-op ":"
+                         prev-no (second elt)))
+              ;; we know we will not delete characters
+              ;; (if associated number is positive, add that many)
+              ((equal (car elt) ">")
+               (setq prev-op ">"
+                     prev-no (second elt)))
+              ;; we know we will delete this many characters
+              ((equal (car elt) "<") (setq prev-op ">"
+                         prev-no (second elt)))
+              ;; we know we will go ahead a certain number
+              ;; of lines but we don't know why yet
+              ((equal (car elt) "|") (setq prev-op "|"
+                         prev-no (second elt)))
+              ;; skip over N characters
+              ;; including |L newlines if previous operator was |
+              ((equal (car elt) "=")
+               (let (fwd)
+                 (if (equal prev-op "|")
+                     ;; TODO: probably need to add a
+                     ;; `next-line' instruction, actually
+                     (setq fwd (+ prev-no (second elt)))
+                   (setq fwd (second elt)))
+                 (setq routine (cons `(forward-char ,fwd) routine))
+               (setq prev-op "="
+                     prev-no (second elt))))
+              ;; add N characters from bank
+              ;; including |L newlines if previous operator was |
+              ((equal (car elt) "+")
+               (let (add)
+                 (if (equal prev-op "|")
+                     (setq add (concat (let ((blanks ""))
+                                         (dotimes (i prev-no)
+                                           (concat blanks "\n"))
+                                         blanks)
+                                       (substring bank 0 (second elt))))
+                   (setq add (substring bank 0 (second elt))))
+                 (setq routine (cons `(insert ,add) routine))
+                 (setq prev-op "+"
+                       prev-no (second elt))))
+              ;; delete N characters
+              ;; including |L newlines if previous operator was |
+              ((equal (car elt) "-")
+               (let (del)
+                 (if (equal prev-op "|")
+                     ;; TODO: again, this probably isn't
+                     ;; what is needed here
+                     (setq del (+ (second elt) prev-no))
+                   (setq del (second elt)))
+                 (setq routine (cons `(delete-char ,del) routine))
+                 (setq prev-op "-"
+                         prev-no (second elt))))
+              ;; apply the give attribute to the next add
+              ;; or keep operation (we can build up a
+              ;; series of attributes to apply)... ignore for now
+              ((equal (car elt) "*") (setq prev-op "*"
+                         prev-no (second elt)))
+              (t nil)))
+          (nbutlast instructions 1))
+    (reverse routine)))
+
+;; it would be nice if changes that were inserted via
+;; linepad were not undoable by the local user.  I'm
+;; guessing that Jan has things set up that way in Rudel.
+;; Can check.
+(defun linepad-apply-changeset (changeset)
+  (save-excursion (set-buffer (get-buffer-create "*Linepad*"))
+                  (goto-char (point-min))
+                  (eval `(progn ,@(linepad-changeset-to-elisp changeset)))))
+
+; (linepad-apply-changeset "Z:1v>5|2=17=m*0+5$auoeu")
 
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ;; end of section for managing changesets
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 ;;; linepad.el ends here
